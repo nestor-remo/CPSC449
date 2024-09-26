308b2a5f8e89ad07382f35a41922f443e9310834
nestor-remo <nestiray12345@gmail.com>
2024-09-25 19:39:43 -0700

    ass1 done

--- ASS1/hello.py
+++ ASS1/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,5 +9,22 @@ name = "Nestor"
 def hello():
     return f"Hello, {name}!"
 
+
+@app.route("/json", methods=["POST"])
+def user():
+    data = request.get_json()
+    if len(data) < 5:
+        return jsonify({"error": "Enter at least 5 fields of data"}), 400
+    return jsonify(data), 200
+
+@app.route("/mixed", methods=["GET", "POST"])
+def combined():
+    if request.method == "GET":
+        return "This is a GET request"
+    elif request.method == "POST":
+        data = request.get_json()
+        return jsonify(data), 200
+
 if __name__ == "__main__":
     app.run(debug=True)
+
